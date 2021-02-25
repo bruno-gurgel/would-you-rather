607e261a851d362084efa9fff73c43d4dbe8dc77
Bruno Gurgel <bmg1612@gmail.com>
2021-02-25 16:59:28 -0300

    feat: Added reducer and action to handle new user sign up

--- src/redux/modules/users.js
+++ src/redux/modules/users.js
@@ -1,14 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { hideLoading, showLoading } from "react-redux-loading";
+import { saveUser } from "../../utils/api";
 
 const userSlice = createSlice({
   name: "users",
   initialState: {},
   reducers: {
     receiveUsers: (state, action) => (state = action.payload),
+    addUser: (state, action) => {
+      const payload = action.payload;
+      return { ...state, payload };
+    },
   },
 });
 
-export const { receiveUsers } = userSlice.actions;
+export function handleNewUser(name, username) {
+  return (dispatch) => {
+    dispatch(showLoading());
+    console.log("redux user");
+    return saveUser({ name, username })
+      .then((newUserInfo) => dispatch(addUser(newUserInfo)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
+
+export const { receiveUsers, addUser } = userSlice.actions;
 
 export const selectUsers = (state) => state.users;
 
