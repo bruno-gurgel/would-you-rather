6453e114544aa7ce99c2702e72fd86806a11a4a7
Bruno Gurgel <bmg1612@gmail.com>
2021-02-26 17:25:52 -0300

    feat: Added PreviewCard component

---
+++ src/components/PreviewCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function PreviewCard(props) {
+  return (
+    <li className="contact-list-item">
+      <div className="contact-avatar" />
+      <div className="contact-details">
+        <h5 className="card-header text-left ">{props.questionAuthor} asks:</h5>
+        <h6 className="text-left pt-3">Would you rather:</h6>
+        <p className="font-italic">...{props.questionPreview}...</p>
+        <Button className="w-50 button" variant="success">
+          View Poll
+        </Button>
+      </div>
+    </li>
+  );
+}
