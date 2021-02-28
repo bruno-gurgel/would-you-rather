92c7b337973b8dc3970ff01a7594f47875ad13cc
Bruno Gurgel <bmg1612@gmail.com>
2021-02-28 18:08:22 -0300

    feat: Filtered answered/unanswered questions to show ->
    - Set the funcitonality that will get the authedUser answered questions
    and display them in case the respective button is clicked and it does
    the same with the unanswered questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,43 +1,67 @@
 import Button from "react-bootstrap/Button";
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row } from "react-bootstrap";
 import PreviewCard from "./PreviewCard";
 import { useSelector } from "react-redux";
 import { getQuestions } from "../redux/modules/questions";
 import { getUsers } from "../redux/modules/users";
+import { getAuthedUser } from "../redux/modules/authedUser";
 
 export default function Dashboard() {
   const questions = useSelector(getQuestions);
   const users = useSelector(getUsers);
+  const authedUser = useSelector(getAuthedUser);
 
-  console.log({ users });
-  console.log({ questions });
+  const [viewAnsweredQuestions, updateViewAnsweredQuestions] = useState(false);
+
+  const answeredQuestions = Object.keys(users[authedUser].answers);
+
+  const questionsNotAnswered = Object.keys(questions).filter(
+    (question) => !answeredQuestions.includes(question)
+  );
+
+  const returnPreviewCards = (questionsArray) => {
+    return (
+      <ul className="list-group contact-list">
+        {questionsArray.map((questionID) => {
+          const questionAuthor = users[questions[questionID].author].name;
+          const questionPreview = questions[questionID].optionOne.text;
+          return (
+            <PreviewCard
+              key={questionID}
+              questionAuthor={questionAuthor}
+              questionPreview={questionPreview}
+            />
+          );
+        })}
+      </ul>
+    );
+  };
 
   return (
     <div className="dashboard">
       <Container fluid="md">
         <Row>
-          <Button className="w-50" variant="info" active={true}>
+          <Button
+            className="w-50"
+            variant="info"
+            active={!viewAnsweredQuestions}
+            onClick={() => updateViewAnsweredQuestions(false)}
+          >
             Unanswered Questions
           </Button>
-          <Button className="w-50" variant="info" active={false}>
+          <Button
+            className="w-50"
+            variant="info"
+            active={viewAnsweredQuestions}
+            onClick={() => updateViewAnsweredQuestions(true)}
+          >
             Answered Questions
           </Button>
         </Row>
-        <ul className="list-group contact-list">
-          {Object.keys(questions).map((questionID) => {
-            const questionAuthor = users[questions[questionID].author].name;
-            const questionPreview = questions[questionID].optionOne.text;
-
-            return (
-              <PreviewCard
-                key={questionID}
-                questionAuthor={questionAuthor}
-                questionPreview={questionPreview}
-              />
-            );
-          })}
-        </ul>
+        {viewAnsweredQuestions
+          ? returnPreviewCards(answeredQuestions)
+          : returnPreviewCards(questionsNotAnswered)}
       </Container>
     </div>
   );
