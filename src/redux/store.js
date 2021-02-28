75bb71476cdcd9aa5c5aca1279d77d2fb284c657
Bruno Gurgel <bmg1612@gmail.com>
2021-02-28 17:56:39 -0300

    feat: Added loading bar

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authedUserReducer from "./modules/authedUser";
 import usersReducer from "./modules/users";
 import questionsReducer from "./modules/questions";
+import { loadingBarReducer } from "react-redux-loading-bar";
 
 export default configureStore({
   reducer: {
     authedUser: authedUserReducer,
     users: usersReducer,
     questions: questionsReducer,
+    loadingBar: loadingBarReducer,
   },
   devTools: true,
 });
