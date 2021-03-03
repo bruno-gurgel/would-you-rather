aa8695d144cb9c9ef0c3ccb964ba96aaf2e84748
Bruno Gurgel <bmg1612@gmail.com>
2021-03-03 11:08:18 -0300

    feat: Refactored code and updated Routes ->
    - Added private route to only authorize access to some components if the
    user is authenticated;
    - Added Switch routes so that it only renders one component and if none
    matches, it will render PageNotFound

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from "react";
 import "../App.css";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchAllData } from "../redux/modules/shared";
-import Authentication from "./Authentication";
-import Dashboard from "./Dashboard";
-import Navigation from "./Navigation";
-import NewQuestion from "./NewQuestion";
-import { Redirect, Route } from "react-router-dom";
-import { getAuthedUser } from "../redux/modules/authedUser";
 import LoadingBar from "react-redux-loading-bar";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
+import { fetchAllData } from "../redux/modules/shared";
+import { getAuthedUser } from "../redux/modules/authedUser";
+import Authentication from "./Authentication";
+import Navigation from "./Navigation";
+import Dashboard from "./Dashboard";
+import NewQuestion from "./NewQuestion";
 import Leaderboard from "./Leaderboard";
-import Pool from "./Pool";
+import Poll from "./Poll";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
 
   const authedUser = useSelector(getAuthedUser);
+  const [isPageNotFound, updateIsPageNotFound] = useState(false);
 
   useEffect(() => {
     dispatch(fetchAllData());
@@ -24,18 +27,26 @@ function App() {
   return (
     <div className="App">
       <LoadingBar />
-      <Route exact path="/" component={Authentication} />
-      {authedUser === null ? (
-        <Redirect to="/" />
-      ) : (
-        <div>
-          <Navigation authedUser={authedUser} />
-          <Route path="/home" component={Dashboard} />
-          <Route path="/add" component={NewQuestion} />
-          <Route path="/leaderboard" component={Leaderboard} />
-          <Route path="/questions/:id" component={Pool} />
-        </div>
-      )}
+      <Route
+        path="/"
+        render={() => (!isPageNotFound && authedUser ? <Navigation /> : null)}
+      />
+      <Switch>
+        <Route path="/auth" component={Authentication} />
+        <PrivateRoute path="/add" component={NewQuestion} />
+        <PrivateRoute path="/leaderboard" component={Leaderboard} />
+        <PrivateRoute path="/questions/:id" component={Poll} />
+        <PrivateRoute exact path="/" component={Dashboard} />
+        <Route
+          path="*"
+          render={(props) => (
+            <PageNotFound
+              {...props}
+              updateIsPageNotFound={updateIsPageNotFound}
+            />
+          )}
+        />
+      </Switch>
     </div>
   );
 }
